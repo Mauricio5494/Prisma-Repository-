335a22dd9171c601b715cc2c20f231037c7896d1
Mauricio5494 <mauriciogorilluberas@gmail.com>
2023-09-20 00:59:10 -0300

    Create DataBaseConnect.cs

---
+++ Etech Program/Diseño/DataBaseConnect.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Diseño
+{
+    class DataBaseConnect
+    {
+        public static MySqlConnection Conectarse()
+        {
+            //requisitos para la conexion (informacion del base de datos):
+            string uid = "root";
+            string pwd = "";
+            string servidor = "localhost";
+            string port = "3306";
+            string database = "etech_db";
+            string requisitos = $"Server={servidor}; Uid={uid}; Pwd={pwd}; Port={port}; Database={database}";
+            //instancia del metodo:
+            MySqlConnection ConnectionDB = new MySqlConnection(requisitos);
+
+            try
+            {
+                return ConnectionDB;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error al conectar a la Base de Datos\n\nMensaje de Error:\n" + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Mensaje técnico:\n\n" + e.Message, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+        }
+    }
+}
